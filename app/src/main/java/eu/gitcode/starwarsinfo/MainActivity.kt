deff40b5f267d5fb667dc70b452a152fdc31b1b2
marcingrabowski <marcingrabowski91@gmail.com>
2020-03-28 19:06:41 +0100

    Initial commit

---
+++ app/src/main/java/eu/gitcode/starwarsinfo/MainActivity.kt
@@ -0,0 +1,18 @@
+package eu.gitcode.starwarsinfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import eu.gitcode.starwarsinfo.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
